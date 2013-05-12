3000bac52d335634f7fdca8cd73cd791f18e50d6
Huon Wilson <dbau.pp+github@gmail.com>
2013-05-12 12:52:02 +1000

    Generate benchmarks via macros, now that that is supported

--- rng/mod.rs
+++ rng/mod.rs
@@ -50,13 +50,11 @@ impl Rng for StdRng {
     }
 }
 
-
 #[cfg(test)]
 mod bench {
     extern mod std;
     use super::*;
     use core::num::Zero;
-
     macro_rules! bench_rng {
         ($rng:ident, $ty:ty) => {
             {
@@ -64,201 +62,64 @@ mod bench {
                 let mut sum: $ty = Zero::zero();
 
                 do b.iter {
-                    for 100.times {
+                    for (4*1024/sys::size_of::<$ty>()).times {
                         sum += rng.gen();
                     }
                 }
+                // avoid dead code elimination
                 if sum.is_zero() {
                     return ();
+                }
+            }
+        };
+        ($rng:ident) => {
+            mod $rng {
+                extern mod std;
+                use super::super::*;
+                use core::num::Zero;
+
+                #[bench]
+                fn u32(b: &mut std::test::BenchHarness) {
+                    bench_rng!($rng, u32)
+                }
+                #[bench]
+                fn u64(b: &mut std::test::BenchHarness) {
+                    bench_rng!($rng, u64)
+                }
+                #[bench]
+                fn f32(b: &mut std::test::BenchHarness) {
+                    bench_rng!($rng, f32)
+                }
+                #[bench]
+                fn f64(b: &mut std::test::BenchHarness) {
+                    bench_rng!($rng, f64)
+                }
+
+                #[bench]
+                fn fill(b: &mut std::test::BenchHarness) {
+                    let mut rng = $rng::new();
+                    let mut vec = ~[0u32, .. 1024];
+                    do b.iter {
+                        rng.fill_vec(vec);
+                    }
                 }
             }
         }
     }
 
-    #[bench]
-    fn xorshift_u32(b: &mut std::test::BenchHarness){
-        bench_rng!(XorShiftRng, u32);
-    }
-    #[bench]
-    fn xorshift_u64(b: &mut std::test::BenchHarness){
-        bench_rng!(XorShiftRng, u64);
-    }
-    #[bench]
-    fn xorshift_f32(b: &mut std::test::BenchHarness){
-        bench_rng!(XorShiftRng, f32);
-    }
-    #[bench]
-    fn xorshift_f64(b: &mut std::test::BenchHarness){
-        bench_rng!(XorShiftRng, f64);
-    }
+    bench_rng! { XorShiftRng }
 
-    #[bench]
-    fn isaac_u32(b: &mut std::test::BenchHarness){
-        bench_rng!(IsaacRng, u32);
-    }
-    #[bench]
-    fn isaac_u64(b: &mut std::test::BenchHarness){
-        bench_rng!(IsaacRng, u64);
-    }
-    #[bench]
-    fn isaac_f32(b: &mut std::test::BenchHarness){
-        bench_rng!(IsaacRng, f32);
-    }
-    #[bench]
-    fn isaac_f64(b: &mut std::test::BenchHarness){
-        bench_rng!(IsaacRng, f64);
-    }
+    bench_rng! { IsaacRng }
+    bench_rng! { Isaac64Rng }
+    bench_rng! { StdRng }
 
-    #[bench]
-    fn isaac64_u32(b: &mut std::test::BenchHarness){
-        bench_rng!(Isaac64Rng, u32);
-    }
-    #[bench]
-    fn isaac64_u64(b: &mut std::test::BenchHarness){
-        bench_rng!(Isaac64Rng, u64);
-    }
-    #[bench]
-    fn isaac64_f32(b: &mut std::test::BenchHarness){
-        bench_rng!(Isaac64Rng, f32);
-    }
-    #[bench]
-    fn isaac64_f64(b: &mut std::test::BenchHarness){
-        bench_rng!(Isaac64Rng, f64);
-    }
+    bench_rng! { MT19937 }
+    bench_rng! { MT19937_64 }
 
-    #[bench]
-    fn mt19937_u32(b: &mut std::test::BenchHarness){
-        bench_rng!(MT19937, u32);
-    }
-    #[bench]
-    fn mt19937_u64(b: &mut std::test::BenchHarness){
-        bench_rng!(MT19937, u64);
-    }
-    #[bench]
-    fn mt19937_f32(b: &mut std::test::BenchHarness){
-        bench_rng!(MT19937, f32);
-    }
-    #[bench]
-    fn mt19937_f64(b: &mut std::test::BenchHarness){
-        bench_rng!(MT19937, f64);
-    }
+    bench_rng! { LFSR258 }
+    bench_rng! { WELL512 }
 
-    #[bench]
-    fn mt19937_64_u32(b: &mut std::test::BenchHarness){
-        bench_rng!(MT19937_64, u32);
-    }
-    #[bench]
-    fn mt19937_64_u64(b: &mut std::test::BenchHarness){
-        bench_rng!(MT19937_64, u64);
-    }
-    #[bench]
-    fn mt19937_64_f32(b: &mut std::test::BenchHarness){
-        bench_rng!(MT19937_64, f32);
-    }
-    #[bench]
-    fn mt19937_64_f64(b: &mut std::test::BenchHarness){
-        bench_rng!(MT19937_64, f64);
-    }
-
-    #[bench]
-    fn lfsr258_u32(b: &mut std::test::BenchHarness){
-        bench_rng!(LFSR258, u32);
-    }
-    #[bench]
-    fn lfsr258_u64(b: &mut std::test::BenchHarness){
-        bench_rng!(LFSR258, u64);
-    }
-    #[bench]
-    fn lfsr258_f32(b: &mut std::test::BenchHarness){
-        bench_rng!(LFSR258, f32);
-    }
-    #[bench]
-    fn lfsr258_f64(b: &mut std::test::BenchHarness){
-        bench_rng!(LFSR258, f64);
-    }
-
-    #[bench]
-    fn well512_u32(b: &mut std::test::BenchHarness){
-        bench_rng!(WELL512, u32);
-    }
-    #[bench]
-    fn well512_u64(b: &mut std::test::BenchHarness){
-        bench_rng!(WELL512, u64);
-    }
-    #[bench]
-    fn well512_f32(b: &mut std::test::BenchHarness){
-        bench_rng!(WELL512, f32);
-    }
-    #[bench]
-    fn well512_f64(b: &mut std::test::BenchHarness){
-        bench_rng!(WELL512, f64);
-    }
-
-    #[bench]
-    fn std_u32(b: &mut std::test::BenchHarness){
-        bench_rng!(StdRng, u32);
-    }
-    #[bench]
-    fn std_u64(b: &mut std::test::BenchHarness){
-        bench_rng!(StdRng, u64);
-    }
-    #[bench]
-    fn std_f32(b: &mut std::test::BenchHarness){
-        bench_rng!(StdRng, f32);
-    }
-    #[bench]
-    fn std_f64(b: &mut std::test::BenchHarness){
-        bench_rng!(StdRng, f64);
-    }
-
-    #[bench]
-    fn cmwc_u32(b: &mut std::test::BenchHarness){
-        bench_rng!(CMWCRng, u32);
-    }
-    #[bench]
-    fn cmwc_u64(b: &mut std::test::BenchHarness){
-        bench_rng!(CMWCRng, u64);
-    }
-    #[bench]
-    fn cmwc_f32(b: &mut std::test::BenchHarness){
-        bench_rng!(CMWCRng, f32);
-    }
-    #[bench]
-    fn cmwc_f64(b: &mut std::test::BenchHarness){
-        bench_rng!(CMWCRng, f64);
-    }
-
-    #[bench]
-    fn minstd_rand_u32(b: &mut std::test::BenchHarness){
-        bench_rng!(MinStd_Rand, u32);
-    }
-    #[bench]
-    fn minstd_rand_u64(b: &mut std::test::BenchHarness){
-        bench_rng!(MinStd_Rand, u64);
-    }
-    #[bench]
-    fn minstd_rand_f32(b: &mut std::test::BenchHarness){
-        bench_rng!(MinStd_Rand, f32);
-    }
-    #[bench]
-    fn minstd_rand_f64(b: &mut std::test::BenchHarness){
-        bench_rng!(MinStd_Rand, f64);
-    }
-
-    #[bench]
-    fn rand48_u32(b: &mut std::test::BenchHarness){
-        bench_rng!(Rand48, u32);
-    }
-    #[bench]
-    fn rand48_u64(b: &mut std::test::BenchHarness){
-        bench_rng!(Rand48, u64);
-    }
-    #[bench]
-    fn rand48_f32(b: &mut std::test::BenchHarness){
-        bench_rng!(Rand48, f32);
-    }
-    #[bench]
-    fn rand48_f64(b: &mut std::test::BenchHarness){
-        bench_rng!(Rand48, f64);
-    }
+    bench_rng! { CMWCRng }
+    bench_rng! { MinStd_Rand }
+    bench_rng! { Rand48 }
 }
