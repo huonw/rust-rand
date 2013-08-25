3f6164935228ee2b8ab367a7022d9d534810c5b3
Huon Wilson <dbau.pp+github@gmail.com>
2013-08-25 22:32:29 +1000

    Implement RDRAND via asm!; asm! is possibly too broken for this to be usable.

--- rng/hardware.rs
+++ rng/hardware.rs
@@ -11,43 +11,52 @@ pub struct CPURng {
     priv force_use_of_new: ()
 }
 
-static NEXT_U64_ATTEMPTS: uint = 3;
+#[cfg(target_arch="x86_64")]
+static RDRAND_CPUID_BIT: u64 = 1 << 30;
 
 #[cfg(target_arch="x86_64")]
 impl CPURng {
     /// Check if the CPU has an RNG.
     pub fn is_supported() -> bool {
         // XXX: use cpuid to check
-        true
+        let mut rcx = 0;
+        unsafe {
+            asm!("cpuid"
+                 : "={rcx}"(rcx)
+                 : "{rax}"(1)
+                 : "{rax}", "{rbx}", "{rdx}"); // clobbered registers
+        }
+        (rcx & RDRAND_CPUID_BIT) != 0
     }
 
     /// Create a new CPURng, returning `None` if it is not supported.
     pub fn try_new() -> Option<CPURng> {
         if CPURng::is_supported() {Some(Rng::new())} else {None}
     }
-
-    /// Retrieve a `u64` without checking for entropy exhaustion.
-    pub fn unchecked_next_u64(&mut self) -> u64 {
-        // XXX: this aint random
-        0
-    }
-
 }
 
 #[cfg(target_arch="x86_64")]
 impl Rng for CPURng {
     fn new() -> CPURng {
-        if !CPURng::is_supported() {
-            fail!("CPURng is not supported")
-        }
-        CPURng { force_use_of_new: () }
+        CPURng::try_new().expect("CPURng not supported")
     }
     fn next_u64(&mut self) -> u64 {
+        static NEXT_U64_ATTEMPTS: uint = 3;
+
         for _ in range(0, NEXT_U64_ATTEMPTS) {
-            let r = self.unchecked_next_u64();
-            // XXX: check that that was actually a random number
-            if true { return r; }
+            let mut rand = 0u64;
+            let mut ok = 0u8;
+
+            unsafe {
+                asm!("rdrand $0
+                     setc $1" : "=r"(rand), "=r"(ok));
+            }
+            if ok == 1 { return rand; }
         }
         fail!("CPURng failed %u times in row.", NEXT_U64_ATTEMPTS)
     }
+
+    // doesn't consume any entropy at the Rust level.
+    #[inline]
+    fn entropy_u64(&self) -> uint { 0 }
 }
